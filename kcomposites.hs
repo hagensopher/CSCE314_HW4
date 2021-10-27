ff0cbf88fe7c3e5d98dc7019aa03f5e17bb40dc0
hagensopher <hagensopher13@gmail.com>
2021-10-27 11:53:34 -0500

    refatctor numOfFactors to give list of fact

--- kcomposites.hs
+++ kcomposites.hs
@@ -1,8 +1,8 @@
-numOfFactors :: Integer -> Integer
-numOfFactors  n = toInteger (length [x | x <- [2..div n 2 +1], n `mod` x == 0])
+numOfFactors :: Integer -> [Integer]
+numOfFactors  n = [x | x <- [2..div n 2 +1], n `mod` x == 0]
 
 kcomposite :: Integer -> [Integer]
-kcomposite n = [x | x <- [1..], numOfFactors x == n ]
+kcomposite n = [x | x <- [1..], toInteger (length (numOfFactors x)) == n ]
 
 
 -- now we have this start of the hard part 
@@ -18,3 +18,6 @@ isTwoComp listLength (x:xs)
 makeTwoComp :: [Char] -> [Char]
 makeTwoComp myWord = if x then myWord else makeTwoComp (myWord ++ ['x']) 
     where x = isTwoComp ( toInteger (length myWord)) (kcomposite 2)
+
+-- se we know the string is now two composite number , so we can use the two factors to make a string
+--so we should make a 

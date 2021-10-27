fb1af82cc04b334915f926b1328e5bbc23878547
hagensopher <hagensopher13@gmail.com>
2021-10-26 20:37:49 -0500

    end of office hours

--- kcomposites.hs
+++ kcomposites.hs
@@ -3,3 +3,9 @@ numOfFactors  n = toInteger (length [x | x <- [2..div n 2 +1], n `mod` x == 0])
 
 kcomposite :: Integer -> [Integer]
 kcomposite n = [x | x <- [1..], numOfFactors x == n ]
+
+
+-- now we have this start of the hard part 
+--take a string and check if it is two composite
+isTwoComp :: [Char] -> Bool
+isTwoComp myList = toInteger (length myList) `elem` kcomposite 2

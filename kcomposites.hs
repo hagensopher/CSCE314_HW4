d31e2c9cff0acb4b38541d3ab45c64547f8bb2df
hagensopher <hagensopher13@gmail.com>
2021-10-27 22:39:40 -0500

    question 2 encoding works

--- kcomposites.hs
+++ kcomposites.hs
@@ -4,7 +4,6 @@ numOfFactors  n = [x | x <- [2..div n 2 +1], n `mod` x == 0]
 kcomposite :: Integer -> [Integer]
 kcomposite n = [x | x <- [1..], toInteger (length (numOfFactors x)) == n ]
 
-
 -- now we have this start of the hard part 
 --take the length of a list and a list of k-composite (above) and check if it is two composite
 isTwoComp :: Integer -> [Integer]-> Bool
@@ -21,7 +20,6 @@ makeTwoComp myWord = if x then myWord else makeTwoComp (myWord ++ ['x'])
 
 -- se we know the string is now two composite number , so we can use the two factors to make a string
 --so make a list that holds the large factor of the list lenght
-
 rowCreation :: [Char] -> Integer -> [Char]
 rowCreation myWord n = take (fromInteger n) myWord
 
@@ -31,13 +29,15 @@ collumCreation myWord n = [rowCreation (myWord) (sndFactor)] ++ collumCreation (
     where sndFactor = last n
 --use map
 
-
 --it was this simple OMG well this grabs each element in a collum
 combineHeads:: [[Char]] -> [Char]
 combineHeads [] = []
 combineHeads listOfStrings = if head listOfStrings == [] then [] else map head listOfStrings ++ combineHeads (map tail listOfStrings)
---makes the right message but gives a expetion that the tail is null
+--makes the right message but gives a expetion that the hged is null
 
+--function that glues it all together
+anagramEncode :: [Char] -> [Char]
+anagramEncode myList = combineHeads (collumCreation (makeTwoComp myList) (numOfFactors  (toInteger (length (makeTwoComp myList)))))
 
 --example of mapping with getting the first element of each collum in a 2D array
 col :: [[Int]] -> [Int]
